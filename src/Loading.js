e08d62925e58dc0c814971c6792be79150742df1
Asutosh panda <pandasutosh@gmail.com>
2021-04-17 14:21:03 +0530

    version 2.0

--- src/Loading.js
+++ src/Loading.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Spinner from 'react-bootstrap/Spinner';
 import './Loading.css'
-import { SpinnerDotted } from 'spinners-react';
+// import { SpinnerDotted } from 'spinners-react';
 const Loading =()=>{
     return <div className="loading">
-  <SpinnerDotted size={90} thickness={101} speed={155} color="rgba(57, 133, 172, 1)" />
+   {/* <SpinnerDotted size={90} thickness={101} speed={155} color="rgba(57, 133, 172, 1)" /> */}
     </div>
 }
 
